874eb704ca6599c19ceb2ea84ccbc0bff404ea42
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:37:34 +0300

    Add files via upload

---
+++ StackPractice/StackPractice/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DECToBIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(checkBrackets(input));
+        }
+        static bool checkBrackets(string input)
+        {
+            Stack<char> checkStack = new Stack<char>();
+            foreach (var bracket in input)
+            {
+                if (bracket == '(' || bracket == '[' || bracket == '{')
+                {
+                    checkStack.Push(bracket);
+                }
+                else if (bracket == ')' || bracket == ']' || bracket == '}')
+                {
+                    if (checkStack.Count <= 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        char skoba = checkStack.Pop();
+                        switch (bracket)
+                        {
+                            case ')':
+                                if (skoba != '(')
+                                    return false;
+                                break;
+                            case ']':
+                                if (skoba != '[')
+                                    return false;
+                                break;
+                            case '}':
+                                if (skoba != '{')
+                                    return false;
+                                break;
+                        }
+                    }
+                }
+            }
+            if (checkStack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
