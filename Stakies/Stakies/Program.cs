a962b68469f0ae59c3d402fce0e642a7611250b0
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:41:20 +0300

    Add files via upload

---
+++ Stakies/Stakies/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stakies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> result = new Queue<int>();
+            int[] numsToAdd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < nums[0]; i++)
+            {
+                result.Enqueue(numsToAdd[i]);
+            }
+            for (int i = 0; i < nums[1]; i++)
+            {
+                result.Dequeue();
+            }
+            if (result.Contains(nums[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if (!result.Contains(nums[2]) && result.Count > 0)
+            {
+                int smallest = result.Peek();
+                foreach (var num in result)
+                {
+                    if (num < smallest)
+                    {
+                        smallest = num;
+                    }
+                }
+                Console.WriteLine(smallest);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
