8b0c63efe7b45a2699a568f463d0b807f4d93cef
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:36:29 +0300

    Add files via upload

---
+++ Stack - reverse string/Stack - reverse string/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack___reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            string resultString = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            
+            Console.WriteLine(String.Join(null,stack));
+        }
+    }
+}
