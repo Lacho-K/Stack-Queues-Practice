a3de48c0eb8d778037d3f3f72bcec7d01e8842f4
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:28:24 +0300

    Add files via upload

---
+++ queue/queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> people = new Queue<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if(input != "Paid")
+                {
+                    people.Enqueue(input);
+                }
+                else
+                {
+                    foreach (var person in people)
+                    {
+                        Console.WriteLine(person);
+                    }
+                    people.Clear();
+                }
+            }
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
