4443d8de67aa7510ce5298f1110f60a4529ac8ba
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:38:42 +0300

    Add files via upload

---
+++ Stacks and queles - more practice/Stacks and queles - more practice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Stacks_and_queles___more_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<int> qNums = new Queue<int>();
+            Stack<string> stackSigns = new Stack<string>();
+            
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsDigit(input[i]))
+                    {
+                        qNums.Enqueue(int.Parse(input[i].ToString()));
+                       
+                    }
+                    else
+                    {
+                    
+                        stackSigns.Push(input[i].ToString());
+                    }
+                    
+                }
+            Console.Write(string.Join(' ', qNums));
+            Console.Write(string.Join(' ', stackSigns));
+            
+            
+        }
+    }
+}
