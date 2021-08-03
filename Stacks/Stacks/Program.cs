7664a5ce7062db0034d25d9b92ec18f71f2cf401
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:38:17 +0300

    Add files via upload

---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<string> stack = new Stack<string>(input.Split());
+            stack.Reverse();
+            int sum = int.Parse(stack.Pop());
+            while (stack.Count > 0)
+            {                                
+                char sign = char.Parse(stack.Pop());               
+                int num = int.Parse(stack.Pop());
+                if (sign == '+')
+                {
+                    sum += num;
+                }
+                else if (sign == '-')
+                {
+                    sum -= num;
+                }
+                                              
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
